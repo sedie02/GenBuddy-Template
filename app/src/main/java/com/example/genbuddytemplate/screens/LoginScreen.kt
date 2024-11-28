33334531f9e88bc869c3f1ef67fd6e8732bbed66
Scott-K22 <s1160328@student.windesheim.nl>
2024-11-28 11:53:46 +0100

    Icon toegevoegd + alles gemerged

--- app/src/main/java/com/example/genbuddytemplate/screens/LoginScreen.kt
+++ app/src/main/java/com/example/genbuddytemplate/screens/LoginScreen.kt
@@ -140,7 +140,7 @@ fun LoginScreen(onNavigate: (Screen) -> Unit) {
 
 // Login Button
             Button(
-                onClick = { /* Handle login */ },
+                onClick = { onNavigate(Screen.MainChat) },
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(48.dp),
